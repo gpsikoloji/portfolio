ddc71efa4ee604ae97b1d5240f15f8daa84f7387
msmhsnl <msmhsnl@gmail.com>
2022-03-06 00:27:17 +0300

    UPDATE detail pages getStaticProps & getStaticPaths

--- src/pages/hizmetlerimiz/[shortName].js
+++ src/pages/hizmetlerimiz/[shortName].js
@@ -1,29 +1,38 @@
-import { useRouter } from "next/router";
-
 import { Layout } from "../../layout/Layout";
 
 import { servicesData } from "../../constants/constants";
 import { Section } from "../../styles/GlobalComponents";
 
 const ServiceDetail = (props) => {
-  const router = useRouter();
-  const { shortName } = router.query;
-
-  const data = servicesData.filter((x) => x.shortName == shortName);
-
   return (
     <Layout>
       <Section row wrap>
-        {data?.length ? (
+        {props.data?.length ? (
           <div>
-            <div>Image: {data[0].image}</div>
-            <div>Title: {data[0].title}</div>
-            <div>Desc: {data[0].description}</div>
+            <div>Image: {props.data[0].image}</div>
+            <div>Title: {props.data[0].title}</div>
+            <div>Desc: {props.data[0].description}</div>
           </div>
         ) : null}
       </Section>
     </Layout>
   );
 };
+
+export async function getStaticProps({ params: { shortName } }) {
+  const data = servicesData.filter((x) => x.shortName == shortName);
+  return { props: { data } };
+}
+
+export async function getStaticPaths() {
+  const paths = servicesData.map((x) => {
+    return { params: { shortName: x.shortName } };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
 export default ServiceDetail;
