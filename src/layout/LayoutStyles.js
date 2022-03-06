822751c6b0ca5bf6771b285dc1c1061721a7cba2
msmhsnl <msmhsnl@gmail.com>
2022-03-06 23:31:41 +0300

    FIX Layout

--- src/layout/LayoutStyles.js
+++ src/layout/LayoutStyles.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   max-width: 1280px;
   width: 100%;
+  min-height: calc(100vh - 245px);
   margin: auto;
   padding-top: 110px;
   @media ${(props) => props.theme.breakpoints.md} {
